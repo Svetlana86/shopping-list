df71a6c07204be78c0c943c38bbb27c7d6aa7808
Svetlana86 <tararovasveta@gmail.com>
2022-07-05 21:58:09 +0300

    Добавлена кнопка удаления списка

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const inputShopItem = document.querySelector('.input-shop-item');
 const shopList = document.querySelector('.shop-list');
 const buttonAddItem = document.querySelector('.button-add-item');
-
+const buttonDelItem = document.querySelector('.button-del-item');
 
 //добавить в список покупок при нажатии на кнопку
 buttonAddItem.addEventListener('click', function(e){
     let listItem = document.createElement('li');
     listItem.innerHTML = inputShopItem.value;
     shopList.appendChild(listItem);
+});
+
+buttonDelItem.addEventListener('click', function(e){
+    let shopList2 = document.querySelector('.shop-list');
+    let count = shopList2.childElementCount;
+    for(var i = 0; i<count; i++){
+        shopList2.removeChild(shopList2.childNodes[0]);
+    }   
 });
 
 
